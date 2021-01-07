7faadc2aee7e6e5760d96ce06e60c807cf282210
pleonardodp12 <pauloleonardo_dp@outlook.com>
2021-01-07 00:02:46 -0300

    feat: create component FeedImage

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
 # Clone Instagram
 
+<p align="center">
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/pleonardodp/clone-instagram.svg">
+  </a>
+</p>
+
+<p align="center">
+<a href="#rocket-technology">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#ballot_box_with_check-prerequisites">O que precisa?</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+    <a href="#up-getting-started">Iniciar projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+</p>
+
+## [](#technology)🚀 Tecnologias
+-  <a href="https://reactnative.dev/">React Native</a>
+-  <a href="https://reactnavigation.org/">React Navigation v5</a>
+-  <a href="https://github.com/axios/axios">Axios</a>
+-  <a href="https://github.com/software-mansion/react-native-gesture-handler">
+-  <a href="https://styled-components.com/">Styled Components</a>
+
+## [](#prerequisites)☑️ Prerequisites
+-   [Node.js](https://nodejs.org/en/)
+-   [NPM](https://www.npmjs.com/) or [Yarn](https://yarnpkg.com/pt-BR/docs/install)
+
+## [](#getting-started) 🆙 Iniciando projeto
+
+-  Run `npm install`
+-  Run `react-native run android`
+
+---
+
 ```shell
 npm install eslint -D
 npx eslint --init
 npm install prettier eslint-config-prettier eslint-plugin-prettier babel-eslint -D
 ```
+
+---
+
+Leonardo Dias 👋  [Get in touch!](https://www.linkedin.com/in/pleonardodp/)
--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -3,7 +3,17 @@ import { FlatList } from 'react-native';
 
 import api from '../../services/api';
 
-import './styles';
+import {
+  Container,
+  Post,
+  Header,
+  Avatar,
+  Name,
+  Description,
+  Loading,
+} from './styles';
+
+import FeedImage from '../../components/FeedImage';
 
 export default function Feed() {
   const [feed, setFeed] = useState([]);
@@ -12,22 +22,52 @@ export default function Feed() {
 
   async function loadPage() {
     setLoading(true);
-
-    const { data } = await api.get('/feed?_expand=author')
-
-    setLoading(false)
-    setFeed(data)
+    const { data } = await api.get('/feed?_expand=author');
+    setLoading(false);
+    setFeed(data);
   }
 
   useEffect(() => {
-    loadPage()
-  }, [])
+    loadPage();
+  }, []);
 
   async function refreshList() {
-    setRefreshing(true)
+    setRefreshing(true);
     await loadPage();
-    setRefreshing(false)
+    setRefreshing(false);
   }
   return (
+    <Container>
+      <FlatList
+        key="list"
+        data={feed}
+        keyExtractor={(item) => String(item.id)}
+        viewabilityConfig={{
+          viewAreaCoveragePercentThreshold: 10,
+        }}
+        showsVerticalScrollIndicator={false}
+        onRefresh={refreshList}
+        refreshing={refreshing}
+        ListFooterComponent={loading && <Loading />}
+        renderItem={({ item }) => (
+          <Post>
+            <Header>
+              <Avatar source={{ uri: item.author.avatar }} />
+              <Name>{item.author.name}</Name>
+            </Header>
+
+            <FeedImage
+              aspectRatio={item.aspectRatio}
+              smallSource={{ uri: item.image }}
+              source={{ uri: item.small }}
+            />
+
+            <Description>
+              <Name>{item.author.name}</Name> {item.description}
+            </Description>
+          </Post>
+        )}
+      />
+    </Container>
   );
 }
