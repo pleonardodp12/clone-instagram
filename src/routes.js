539f75456810de0b1f29150c520e2bb7377304fa
pleonardodp12 <pauloleonardo_dp@outlook.com>
2021-01-06 22:21:49 -0300

    feat: added Header components, routes configureds, styled-components and axios

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Feed from './pages/Feed';
+import Header from './components/Header';
 
 const Stack = createStackNavigator();
 
@@ -14,8 +15,7 @@ export default function Routes() {
           name="Feed"
           component={Feed}
           options={{
-            title: 'Feed',
-            headerTitleAlign: 'center',
+            headerTitle: () => <Header />,
             headerStyle: {
               backgroundColor: '#f5f5f5',
             },
